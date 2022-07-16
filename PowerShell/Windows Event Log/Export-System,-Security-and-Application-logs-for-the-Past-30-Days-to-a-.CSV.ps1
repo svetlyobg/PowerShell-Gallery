5ffc5786f87bd0038fe0528531741034e396ec47
svetlyobg <swk_12@abv.bg>
2022-07-16 15:49:31 +0300

    Export System, Security and Application logs for the Past 30 Days to a .CSV

---
+++ PowerShell/Windows Event Log/Export-System,-Security-and-Application-logs-for-the-Past-30-Days-to-a-.CSV.ps1
@@ -0,0 +1,36 @@
+﻿Get-EventLog -LogName System -After ((get-date).AddDays(-30)) -EntryType Error , Warning  |  ConvertTo-Csv | Out-File -FilePath C:\Users\Administrator\Desktop\EVENTLOGS\SYSTEMlast30days.csv -Force -Verbose
+
+Get-EventLog -LogName Security -After ((get-date).AddDays(-30)) -EntryType FailureAudit,SuccessAudit |  ConvertTo-Csv | Out-File -FilePath C:\Users\Administrator\Desktop\EVENTLOGS\SECURITYlast30days.csv -Force -Verbose
+
+Get-EventLog -LogName Application -After ((get-date).AddDays(-30)) -EntryType Error, Warning |  ConvertTo-Csv | Out-File -FilePath C:\Users\Administrator\Desktop\EVENTLOGS\APPLICATIONlast30days.csv -Force
+# SIG # Begin signature block
+# MIIFagYJKoZIhvcNAQcCoIIFWzCCBVcCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
+# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQU2BYqGj13lgeUxUshikcM8uWo
+# mcugggMGMIIDAjCCAeqgAwIBAgIQJLzUFQ0UMqZIQD0LynYJzzANBgkqhkiG9w0B
+# AQsFADAZMRcwFQYDVQQDDA5HbyBMaXZlIFVLIEx0ZDAeFw0yMjA3MTYxMTU3MzZa
+# Fw0yNzA3MTUyMTAwMDBaMBkxFzAVBgNVBAMMDkdvIExpdmUgVUsgTHRkMIIBIjAN
+# BgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA1jBbQhei1OZxT2X07woTaSXAAIhc
+# nY1a8OwQdzKLpWKCskGbDmGBH0bdqLq1m+y8jWKnf1tEnzaFVxUKrcwq57hy8og2
+# xK9Ciq2+X43ZpA4acLUnvK0auUNig2vwWXlnPr4vh4g6+E6nMvvyhPA5ND+Uvhm2
+# wSi7Z+qlmYQqm7wI7IEoaGy/8zyPB1vnWOL2UUYL8oTSklGdA6/wPGcV0S41vfGR
+# 1YClE/AaAMB2+GZgkGgw9gQvKwsNRFIJSKu3OHICfrPRJgObdMnKr6F2jFl2LP+E
+# Rf/Qy7BkTMwLIuxm2Tik+yPh3ZLGWGr2TtQoipNcdIg/7jmGuepTssD5/QIDAQAB
+# o0YwRDAOBgNVHQ8BAf8EBAMCB4AwEwYDVR0lBAwwCgYIKwYBBQUHAwMwHQYDVR0O
+# BBYEFDBp69/PtsKtexPQHwwNhwtX73JqMA0GCSqGSIb3DQEBCwUAA4IBAQBQ4+Qm
+# FQd/u421ZKpspyhsYw/Hjnn27uBwJ1420lpQSApuI6BnsmlZlpNVAl5ETMtbzezo
+# x4xwkQ2H4/+FWg4/Mic0adMwAgqGEeOpWYZxpUQFnzCIlyH6QmXvDLuR5FwVUFKt
+# fdbUj5waOOY6UJ3MSGWEykAX3R3jF/g9AgZmfvTeK9ko72QU595uIHRRXubq3akk
+# 8WGiZcvgZ6WwzbHDw3iM3Q16OKOGKC/UQgJ1eiawavJUF7DQawB1VonfnbT9caZB
+# 8H88Qf8u3g2FQlStoZ1lYfIUj37PZfbaBhVVZyMS7FxmSmfLXvQLtV0pT7nSRK63
+# Ki30MG53JtJQdqYmMYIBzjCCAcoCAQEwLTAZMRcwFQYDVQQDDA5HbyBMaXZlIFVL
+# IEx0ZAIQJLzUFQ0UMqZIQD0LynYJzzAJBgUrDgMCGgUAoHgwGAYKKwYBBAGCNwIB
+# DDEKMAigAoAAoQKAADAZBgkqhkiG9w0BCQMxDAYKKwYBBAGCNwIBBDAcBgorBgEE
+# AYI3AgELMQ4wDAYKKwYBBAGCNwIBFTAjBgkqhkiG9w0BCQQxFgQUrj2wCPSSVoAk
+# 86O01B8RqurqJ4kwDQYJKoZIhvcNAQEBBQAEggEAjsS3lM8zluwQhSWGMYKRejNO
+# E+trOs098h4NccWBEWSiboINI+pFitK/Lk9MARjv2YtanA2T4iSmwtUbgn3sEoaJ
+# j6pFjc50HS0vO4NGA5+0aC3yhlapl45LLk1bEwZRDIIGl6NA4gcJjTTO9k4Popzg
+# Df3cGX4Hg/vEmP5wG7yWY7PE8o4vmGE/tDFos0RDIzDQHhG3EmgdXmeTZZv4dKOv
+# 1f2TTFHt+4YFxUIiCH9dIqjVfKLOdRBIX6mVGYdBE49d8d1fzHOA11dZIwhuQ0K0
+# nGtzpYw5C/tfn6Zg/9aUOe3S47dodTG47QUxoab5s6vjot4IaDTIZq/9ZXl1BA==
+# SIG # End signature block
