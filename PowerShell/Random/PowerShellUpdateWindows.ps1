ae8b2b800f15985dd3aa74d2a9f8293f663b5d26
svetlyobg <swk_12@abv.bg>
2022-07-16 15:11:25 +0300

    PowerShellUpdateWindows

---
+++ PowerShell/Random/PowerShellUpdateWindows.ps1
@@ -0,0 +1,70 @@
+Write-Host -ForegroundColor DarkYellow "Installing Install-Module -Name PSWindowsUpdate"
+Write-Host ""
+Install-Module -Name PSWindowsUpdate -Verbose
+Write-Host ""
+Write-Host -ForegroundColor Green "Install complete"
+Write-Host ""
+
+# Please Use Y
+
+Write-Host -ForegroundColor DarkYellow "Import-Module -Name PSWindowsUpdate"
+Write-Host ""
+Import-Module -Name PSWindowsUpdate -Verbose
+Write-Host ""
+Write-Host -ForegroundColor Green "Install complete"
+Write-Host ""
+
+# Please Use Y
+
+Write-Host -ForegroundColor DarkYellow "Showing Windows Updates"
+Write-Host ""
+Write-Host -ForegroundColor Red "!!! PPLEASE USE Y !!!"
+Write-Host -ForegroundColor Red "!!! PPLEASE USE Y !!!"
+Write-Host -ForegroundColor Red "!!! PPLEASE USE Y !!!"
+Write-Host ""
+Show-WindowsUpdate -Verbose
+Write-Host -ForegroundColor Green "Install complete"
+Write-Host ""
+
+# Please Use Y
+
+Write-Host -ForegroundColor DarkYellow "Downloading and Installing Windows Updates"
+Write-Host ""
+Write-Host -ForegroundColor Red "!!! PPLEASE USE Y !!!"
+Write-Host -ForegroundColor Red "!!! PPLEASE USE Y !!!"
+Write-Host -ForegroundColor Red "!!! PPLEASE USE Y !!!"
+Write-Host ""
+Install-WindowsUpdate -Verbose
+Write-Host -ForegroundColor Green "Install complete"
+Write-Host ""
+# SIG # Begin signature block
+# MIIFagYJKoZIhvcNAQcCoIIFWzCCBVcCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
+# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUqM5MEBXIklr1/xwzYHfrgdQ4
+# EG6gggMGMIIDAjCCAeqgAwIBAgIQEtSZ1T/kOptFn60oc6QVGTANBgkqhkiG9w0B
+# AQsFADAZMRcwFQYDVQQDDA5HbyBMaXZlIFVLIEx0ZDAeFw0yMjA3MTYxMTU4MDRa
+# Fw0yNzA3MTUyMTAwMDBaMBkxFzAVBgNVBAMMDkdvIExpdmUgVUsgTHRkMIIBIjAN
+# BgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA6sGeW+q3SYpKYZDPQu7JlI3+EWSP
+# CoyXoSwzgC1SpE6vkEJYv0A4INSi4qRpiZhx7bxEREB9nDEWZTLhnXj55xe/0f6Z
+# 1Xvszi9AZb0S7Hs9rIdPOYI0+FCLLauKShC7re2Gb7Yneh1GOnyLBzilj4tf2NIL
+# SgaVBSS1UOH50i+MXwsBBjtGuIX8mipoGFXz2KQB7RLg0f8B4fM17X/to9/Mvh64
+# LkJ641g9YbfG8hMyV49GgtOamJ8Vs0hc5M5k2T01Xen1ohDH7CT7N2MJ8DZdHWyE
+# T7HCc3DFihGiELCDTL8mSeCW/9y0IZfGe/wXjswN5lumVkmgdyuAW8FEWQIDAQAB
+# o0YwRDAOBgNVHQ8BAf8EBAMCB4AwEwYDVR0lBAwwCgYIKwYBBQUHAwMwHQYDVR0O
+# BBYEFFqWiQL2Ulc7dX4k9KFaUxF615mlMA0GCSqGSIb3DQEBCwUAA4IBAQBVBIBh
+# s++k7Fe4+xqRc5N23lLDBw0+3htnzaFSxHh5geyMH9A5kDyGzcn1JpmeTyWPgdnN
+# ecuL+Pu3GGPGfVP43G9Pb/E1hlh05zDbkqZK7SdxYE3hpjC+RbVoRQu0nrpDhL/x
+# CNwt/WJt7eWT4Z+YRXZSPQMCa3JAMuQoZS9/q5F6npJsoErntRX//x/K6eOCff9I
+# C+txUCaBrPIdjv5nuXqJb96L2zgUsUjg9XOAhPclu+Kl+NGgJF8hk+MyqB+fitkR
+# pKe1GngOZqVCwTzLx56p67b7anhHn1XzNCAewcDjB3miPVR7vYN4ge1T4DBJpI5r
+# bzHQtincz/LM9nLnMYIBzjCCAcoCAQEwLTAZMRcwFQYDVQQDDA5HbyBMaXZlIFVL
+# IEx0ZAIQEtSZ1T/kOptFn60oc6QVGTAJBgUrDgMCGgUAoHgwGAYKKwYBBAGCNwIB
+# DDEKMAigAoAAoQKAADAZBgkqhkiG9w0BCQMxDAYKKwYBBAGCNwIBBDAcBgorBgEE
+# AYI3AgELMQ4wDAYKKwYBBAGCNwIBFTAjBgkqhkiG9w0BCQQxFgQUzM/FINJk+IO3
+# iB/k3OHo55Kghf0wDQYJKoZIhvcNAQEBBQAEggEADC3j4w1uygjwmf04H5ZQJmTL
+# l3vSGIbAn0PI7mLhMgeMSBNWt7fQ5jIye0zGgyvoAm0n+ZEUH81ZqmNNOfc9HEvg
+# +oqNFFcmkpLbh3XmrFNq3ohqhWH8vZjfM3NRxLaVKYdpw2h5qoECBj7CXmS2iS5V
+# YV1wOp+vkRjei/Vcan3w/wUM738D84WI6I/b6rAL5ctt435sbMZ9Wb7nu22F0rgp
+# IYxM8Km2FiAfKnGuYvqK2/dMaa392JE9G9Ibo6L8+kh80ozpUNvizjA+p9uOZZ//
+# h5lCKpYAIoR16AdmIYMzmIRFryZZvNGze2wd7QhDEtA5fo4ERvQvTo3MJyvYgg==
+# SIG # End signature block
