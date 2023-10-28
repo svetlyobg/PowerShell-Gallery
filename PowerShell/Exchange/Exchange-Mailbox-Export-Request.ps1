d2e6594414e9366ec0f4fc342bc7135270ff7316
svetlyobg <swk_12@abv.bg>
2023-10-28 11:42:07 +0300

    Exporting Exchange User Archive Mailbox

--- PowerShell/Exchange/Exchange-Mailbox-Export-Request.ps1
+++ PowerShell/Exchange/Exchange-Mailbox-Export-Request.ps1
@@ -1,5 +1,7 @@
 New-MailboxExportRequest -Mailbox "user@example.com" -FilePath "\\exchange\export\User.pst" -Name "CustomExportName" -Description "Description of the export"
 
+New-MailboxExportRequest -Mailbox "user@example.com" -IsArchive -FilePath "\\exchange\export\UserArchive.pst" -Name "User Archive"
+
 Get-MailboxExportRequest -Mailbox "User Display Name or Mailbox"
 
 Get-MailboxExportRequest -Identity "User Display Name or Mailbox"
