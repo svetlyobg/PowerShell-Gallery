e5c264fc40c00799b866e2769c15c313562fd004
svetlyobg <swk_12@abv.bg>
2023-12-01 14:37:14 +0200

    Check the mail flow using only a subject

--- PowerShell/Exchange/Check-Exchange-Mailbox-Mail-Flow.ps1
+++ PowerShell/Exchange/Check-Exchange-Mailbox-Mail-Flow.ps1
@@ -1,1 +1,6 @@
 Get-MessageTrackingLog -ResultSize Unlimited -Start "11/24/2023 8:00AM" -End "11/27/2023 5:00PM" -Sender "mailbox@example.co.uk" | Format-Table
+#
+# Check the mail flow using only a subject
+#
+Get-MessageTrackingLog -MessageSubject "SUBJECT"
+
